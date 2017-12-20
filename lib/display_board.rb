99d73c83436c84ff10b4c9314498692b83482df2
Veronica Kim <vero.a.kim@gmail.com>
2017-12-20 03:39:55 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
-  puts "#{0}|   |   "
+  puts "#{0}  |#{1}  |   "
   puts "-----------"
   puts "   |   |   "
   puts "-----------"
